fcf9c0900e46ab4bc51b83af8a24276516740ac5
reaperes <emerald105@hanmail.net>
2015-10-04 23:42:30 +0900

    add fb accessToken verification

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 'use strict';
+let https = require('https');
 let app = require('koa')();
 let bodyParser = require('koa-bodyparser');
 let router = require('koa-router')();
 
 app.use(bodyParser());
 
-router.post('/authenticate', function *() {
-  console.log('user name : ' + this.request.body.userName);
-  console.log('access token : ' + this.request.body.accessToken);
-  this.response.status = 200;
-});
+let verify = function (accessToken) {
+  return new Promise(function (resolve, reject) {
+    https.get('https://graph.facebook.com/me?access_token=' + accessToken, response => {
+      resolve(response.statusCode);
+    });
+  })
+};
+
+let authController = function *() {
+    console.log('user name : ' + this.request.body.userName);
+    console.log('access token : ' + this.request.body.accessToken);
+    yield verify(this.request.body.accessToken)
+      .then((statusCode) => {
+        this.response.status = statusCode;
+      });
+};
+
+router.post('/authenticate', authController);
 
 app.use(router.routes())
    .use(router.allowedMethods());
